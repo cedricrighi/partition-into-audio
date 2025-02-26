c1d5ae9e05ad42df89f931e085d9b1735be22907
Gamberge <cedric.righi@gmail.com>
2025-02-26 13:42:39 +0100

    style: finish navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,31 @@
 import { useTranslation } from "react-i18next";
 import MusicSVG from "/icons/music.svg";
+import "../styles/Navbar.css";
 
 export default function Navbar() {
 	const { t, i18n } = useTranslation();
 
 	return (
-		<nav>
-			<img src={MusicSVG} alt="" />
-			<h1>{t("navbarTitle")}</h1>
-			<button type="button" onClick={() => i18n.changeLanguage("fr")}>
-				🇫🇷
-			</button>
-			<button type="button" onClick={() => i18n.changeLanguage("en")}>
-				🇬🇧
-			</button>
+		<nav className="navbar">
+			<img className="navbar-image" src={MusicSVG} alt="" />
+			<h1 className="navbar-site-title">{t("navbarTitle")}</h1>
+			<div className="translate-buttons">
+				<button
+					className="translate-button"
+					type="button"
+					onClick={() => i18n.changeLanguage("fr")}
+				>
+					🇫🇷
+				</button>
+				<p>/</p>
+				<button
+					className="translate-button"
+					type="button"
+					onClick={() => i18n.changeLanguage("en")}
+				>
+					🇬🇧
+				</button>
+			</div>
 		</nav>
 	);
 }
